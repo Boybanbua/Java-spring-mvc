bd18aa6e627c5256162cd56cd9a045de7ad0dcb7
BanBuaBoy <datbanbua2k4@gmail.com>
2024-09-16 20:12:57 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,36 +1,41 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import vn.hoidanit.laptopshop.service.Userservice;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.service.UserService;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping()
-//     public String getHomePage() {
-//         return "Hello Controller";
-//     }
-
-// }
-
-@RestController
+@Controller
 public class UserController {
-    // DEPENDENCY injection
-    private Userservice userservice;
 
-    public UserController(Userservice userservice) {
-        this.userservice = userservice;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @GetMapping()
-    public String getHomePage() {
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("eric", test);
+        model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here" + hoidanit);
+        return "hello";
+    }
 }
--- src/main/java/vn/hoidanit/laptopshop/service/Userservice.java
+++ src/main/java/vn/hoidanit/laptopshop/service/Userservice.java
@@ -3,8 +3,8 @@ package vn.hoidanit.laptopshop.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Userservice {
-    public String hello() {
+public class UserService {
+    public String handleHello() {
         return "Hello WORD";
     }
 }
