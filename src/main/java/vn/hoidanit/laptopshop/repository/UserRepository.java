8ae936da32022660697948203b4de9a8f2acbefa
BanBuaBoy <datbanbua2k4@gmail.com>
2024-09-24 21:21:43 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import vn.hoidanit.laptopshop.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    User save(User hoidanit);
 }
